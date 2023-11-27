7f606b6599663e504457a25091c98030596f66b1
Dipesh Basnet <147070616+dipeshbasnet146@users.noreply.github.com>
2023-11-27 08:21:54 -0500

    Update Sandwich.java
    variables added

--- src/main/java/org/delicious/Sandwich/Sandwich.java
+++ src/main/java/org/delicious/Sandwich/Sandwich.java
@@ -1,4 +1,9 @@
 package org.delicious.Sandwich;
 
 public enum Sandwich {
+    private String size;
+    private String breadType;
+    private List<Topping> toppings;
+
+    // Constructor, getters, setters, and methods
 }
