9ceb72d6d506b557fc9f583d8d38aa472bbb8893
Dipesh Basnet <147070616+dipeshbasnet146@users.noreply.github.com>
2023-11-28 07:56:19 -0500

    updated according to SanwichSize and SandwitchType

--- src/main/java/org/delicious/Sandwich/Sandwich.java
+++ src/main/java/org/delicious/Sandwich/Sandwich.java
@@ -1,60 +1,47 @@
 package org.delicious.Sandwich;
 
-
-
-package org.delicious.Sandwich;
-
+import org.delicious.Model.Sandwich.BreadSize;
+import org.delicious.Model.Sandwich.BreadType;
+import org.delicious.Model.Sandwich.PremiumToppings;
+import org.delicious.Model.Sandwich.RegularToppings;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Sandwich {
-    private String size; // e.g., "4 inch", "8 inch", "12 inch"
-    private String breadType; // e.g., "white", "wheat", "rye", "wrap"
-    private List<Toppings> toppings; // List of Toppings
+    private BreadSize size; // Enum for the size of the sandwich
+    private BreadType breadType; // Enum for the type of bread
+    private List<PremiumToppings> premiumToppings; // List of Premium Toppings
+    private List<RegularToppings> regularToppings; // List of Regular Toppings
 
     // Constructor
-    public Sandwich(String size, String breadType) {
+    public Sandwich(BreadSize size, BreadType breadType) {
         this.size = size;
         this.breadType = breadType;
-        this.toppings = new ArrayList<>(); // Initialize the toppings list
+        this.premiumToppings = new ArrayList<>(); // Initialize the premium toppings list
+        this.regularToppings = new ArrayList<>(); // Initialize the regular toppings list
     }
 
-    // Getter for size
-    public String getSize() {
-        return size;
+    // Getters and setters for size, breadType, premiumToppings, and regularToppings...
+
+    // Method to add a premium topping
+    public void addPremiumTopping(PremiumToppings topping) {
+        this.premiumToppings.add(topping);
     }
 
-    // Setter for size
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    // Getter for breadType
-    public String getBreadType() {
-        return breadType;
-    }
-
-    // Setter for breadType
-    public void setBreadType(String breadType) {
-        this.breadType = breadType;
-    }
-
-    // Getter for toppings
-    public List<Toppings> getToppings() {
-        return toppings;
-    }
-
-    // Method to add a topping
-    public void addTopping(Toppings topping) {
-        toppings.add(topping);
+    // Method to add a regular topping
+    public void addRegularTopping(RegularToppings topping) {
+        this.regularToppings.add(topping);
     }
 
     // Method to calculate the price of the sandwich
     public double calculatePrice() {
-        double price = /* base price based on size and bread type */;
-        for (Toppings topping : toppings) {
-            price += topping.getPrice();
+        double price = breadType.getPrice(); // Base price from the type of bread
+        // Add additional cost for premium toppings
+        for (PremiumToppings topping : premiumToppings) {
+            // Assume a fixed additional cost for premium toppings or calculate as needed
+            price += /* additional cost for premium topping */;
         }
+        // Regular toppings can be assumed to be included in the base price
         return price;
     }
 
@@ -62,9 +49,10 @@ public class Sandwich {
     @Override
     public String toString() {
         return "Sandwich{" +
-               "size='" + size + '\'' +
-               ", breadType='" + breadType + '\'' +
-               ", toppings=" + toppings +
+               "size=" + size +
+               ", breadType=" + breadType +
+               ", premiumToppings=" + premiumToppings +
+               ", regularToppings=" + regularToppings +
                '}';
     }
 }
