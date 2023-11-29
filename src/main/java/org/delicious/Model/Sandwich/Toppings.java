803cc6e9c5066cebb8fc43806d1470b6ffd4e940
Sahara <sahara.tm65@gmail.com>
2023-11-28 22:37:39 -0500

    Removed unnessary comments to improve readability in Toppings class

--- src/main/java/org/delicious/Model/Sandwich/Toppings.java
+++ src/main/java/org/delicious/Model/Sandwich/Toppings.java
@@ -2,47 +2,38 @@ package org.delicious.Model.Sandwich;
 
 // Dipesh's Code
 public class Toppings {
-
     private double price;
 
-    // Constructor
     public Toppings(String name, ToppingType type, double price) {
         this.name = name;
         this.type = type;
         this.price = price;
     }
 
-    // Public Getter for name
     public String getName() {
         return name;
     }
 
-    // Public Setter for name
     public void setName(String name) {
         this.name = name;
     }
 
-    // Public Getter for type
     public ToppingType getType() {
         return type;
     }
 
-    // Public Setter for type
     public void setType(ToppingType type) {
         this.type = type;
     }
 
-    // Public Getter for price
     public double getPrice() {
         return price;
     }
 
-    // Public Setter for price
     public void setPrice(double price) {
         this.price = price;
     }
 
-    // Method to check if the topping is premium
     public boolean isPremium() {
         return type == ToppingType.MEAT || type == ToppingType.CHEESE;
     }
