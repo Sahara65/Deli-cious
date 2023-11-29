5f4d639a6ba1c5468efd71b219118e0d598fa119
dipeshbasnet146 <munnabasnet146@gmail.com>
2023-11-29 09:34:14 -0500

    Separated savaReceipt from Checkout.java and placed inside ReceiptManager.java

--- src/main/java/org/delicious/Model/IO/ReceiptManager.java
+++ src/main/java/org/delicious/Model/IO/ReceiptManager.java
@@ -1,6 +1,24 @@
+
 package org.delicious.Model.IO;
+
+        import java.io.BufferedWriter;
+        import java.io.FileWriter;
+        import java.io.IOException;
+        import java.time.LocalDateTime;
+        import java.time.format.DateTimeFormatter;
 
 public class ReceiptManager {
 
-}
+    public static void saveReceipt(String orderSummary) throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String formattedDateTime = now.format(formatter);
+        String fileName = "Receipts/" + formattedDateTime + ".txt";
 
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(orderSummary);
+        } catch (IOException e) {
+            throw new IOException("Error writing to receipt file: " + e.getMessage(), e);
+        }
+    }
+}
--- src/main/java/org/delicious/Model/Order/Checkout.java
+++ src/main/java/org/delicious/Model/Order/Checkout.java
@@ -1,14 +1,11 @@
+// Dipesh's Code
 package org.delicious.Model.Order;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import org.delicious.Model.IO.ReceiptManager;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
-// Dipesh's Code
 public class Checkout {
-    private final Order currentOrder;
+    private Order currentOrder;
 
     public Checkout(Order order) {
         this.currentOrder = order;
@@ -18,29 +15,11 @@ public class Checkout {
         String orderSummary = currentOrder.getOrderSummary();
 
         try {
-            saveReceipt(orderSummary);
+            ReceiptManager.saveReceipt(orderSummary);
         } catch (IOException e) {
             System.out.println("Error saving receipt: " + e.getMessage());
-
         }
 
         return orderSummary;
-    }
-
-    private void saveReceipt(String orderSummary) throws IOException {
-
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
-        String formattedDateTime = now.format(formatter);
-
-
-        String fileName = formattedDateTime + ".txt";
-
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            writer.write(orderSummary);
-        } catch (IOException e) {
-            throw new IOException("Error writing to receipt file: " + e.getMessage(), e);
-        }
     }
 }
